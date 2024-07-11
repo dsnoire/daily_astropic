82e5f009938508ae3c00eeefb4039be9b3e57084
Kuba <kosinski.jakub01@gmail.com>
2024-07-11 18:49:45 +0200

    add internet permission to release mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:daily_astropic/astro_picture/presentation/bloc/local_astro_picture_cubit/local_astro_picture_cubit.dart';
 import 'package:daily_astropic/utils/di/service_locator.dart';
 import 'package:daily_astropic/utils/router/app_router.dart';
+import 'package:daily_astropic/utils/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -8,7 +9,6 @@ import 'package:google_fonts/google_fonts.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeServiceLocator();
-
   runApp(const MyApp());
 }
 
@@ -22,16 +22,8 @@ class MyApp extends StatelessWidget {
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
         title: 'Daily Astropic',
-        routerConfig: appRouter,
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color(0xFF673AB7),
-            brightness: Brightness.dark,
-          ),
-          useMaterial3: true,
-          scaffoldBackgroundColor: const Color(0xFF0A0A0A),
-          textTheme: GoogleFonts.spaceGroteskTextTheme(),
-        ),
+        routerConfig: AppRouter.router,
+        theme: AppTheme.theme,
       ),
     );
   }
--- lib/utils/router/app_router.dart
+++ lib/utils/router/app_router.dart
@@ -5,26 +5,28 @@ import 'package:daily_astropic/astro_picture/presentation/views/home_view.dart';
 import 'package:daily_astropic/astro_picture/presentation/views/picture_details_view.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(
-  initialLocation: '/home',
-  routes: [
-    GoRoute(
-      path: '/home',
-      builder: (context, state) => const HomeView(),
-    ),
-    GoRoute(
-      path: '/picture-details',
-      builder: (context, state) => const PictureDetailsView(),
-    ),
-    GoRoute(
-      path: '/favourites',
-      builder: (context, state) => const FavouritesView(),
-    ),
-    GoRoute(
-      path: '/favourites-details',
-      builder: (context, state) => FavouritesDetailsView(
-        picture: state.extra as Picture,
+class AppRouter {
+  static final router = GoRouter(
+    initialLocation: '/home',
+    routes: [
+      GoRoute(
+        path: '/home',
+        builder: (context, state) => const HomeView(),
       ),
-    ),
-  ],
-);
+      GoRoute(
+        path: '/picture-details',
+        builder: (context, state) => const PictureDetailsView(),
+      ),
+      GoRoute(
+        path: '/favourites',
+        builder: (context, state) => const FavouritesView(),
+      ),
+      GoRoute(
+        path: '/favourites-details',
+        builder: (context, state) => FavouritesDetailsView(
+          picture: state.extra as Picture,
+        ),
+      ),
+    ],
+  );
+}
